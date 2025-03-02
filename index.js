5dad2ca99bc20a63e9e390fdcf2c5de58172ee8f
Parvaggarawal01 <parvaggarwal130@gmail.com>
2025-03-02 17:09:08 +0530

    adnin Router

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const {userRouter} = require("./routes/user");
 const {courseRouter} = require("./routes/course");
+const {adminRouter} = require("./routes/admin");
 
 app.use(express.json())
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
+app.use("/api/v1/admin", adminRouter);
 
 
 app.listen(3000);
---
+++ routes/admin.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const adminRouter = express.Router();
+
+
+adminRouter.post("/signup", (req, res) => {
+  res.json({
+    message: "Signup Endpoint",
+  });
+});
+
+adminRouter.post("/login", (req, res) => {
+  res.json({
+    message: "Login Endpoint",
+  });
+});
+
+adminRouter.post("/course", (req, res) => {
+  res.json({
+    message: "Course Endpoint",
+  })
+})
+
+adminRouter.put("/course", (req, res) => {
+  res.json({
+    message: "Course Endpoint",
+  })
+})
+
+adminRouter.get("/course", (req, res) => {
+  res.json({
+    message: "Course Endpoint",
+  })
+})
+
+module.exports = {
+  adminRouter: adminRouter,
+}
