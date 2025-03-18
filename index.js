5d32796accf8cd993d1e87dab39eacaba8d45921
Parvaggarawal01 <parvaggarwal130@gmail.com>
2025-03-18 23:40:15 +0530

    Adding Nodemon and changes package.json a little bit

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use("/api/v1/admin", adminRouter);
 const main = async() => {
   await mongoose.connect(process.env.MONGO_URL);
   app.listen(3000);
+  console.log("listening on port 3000")
 }
 
 main();
