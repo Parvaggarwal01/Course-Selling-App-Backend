a1af1978b42bc18e65d46a6af9ad02ce48eacc6d
Parvaggarawal01 <parvaggarwal130@gmail.com>
2025-03-31 01:38:29 +0530

    Creating Post end point for creating course in admin

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const adminRouter = express.Router();
-const {adminModel} = require("../db");
+const {adminModel, courseModel} = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_ADMIN_PASSWORD } = require("../config");
 const bcrypt = require("bcrypt");
+const { adminMiddleware } = require('../middleware/admin');
 
 adminRouter.post("/signup", async(req, res) => {
   const { email, password, firstName, lastName } = req.body;
@@ -61,9 +62,20 @@ adminRouter.post("/login", async(req, res) => {
   }
 });
 
-adminRouter.post("/course", (req, res) => {
+adminRouter.post("/course",adminMiddleware, async(req, res) => {
+  const adminId = req.userId
+
+  const { title, description, imageUrl, price } = req.body;
+  await courseModel.create({
+    title: title,
+    description: description,
+    imageUrl: imageUrl,
+    price: price,
+    creatorId: adminId
+  })
   res.json({
-    message: "Course Endpoint",
+    message: "Course Created",
+    courseId: course_id
   })
 })
 
